67257bb4542ed9c5ce639e0ca19aa6cfe17d515b
EdwinPB <epolo@wawand.co>
2023-03-11 11:03:36 -0500

    test ngrok

--- app/routes.go
+++ app/routes.go
@@ -1,11 +1,13 @@
 package app
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 
-	"ineedApp/public"
 	"ineedApp/app/actions/home"
 	"ineedApp/app/middleware"
+	"ineedApp/public"
 
 	"github.com/gobuffalo/buffalo"
 )
@@ -18,5 +20,15 @@ func setRoutes(root *buffalo.App) {
 	root.Use(middleware.CSRF)
 
 	root.GET("/", home.Index)
+	root.POST("/test/", func(c buffalo.Context) error {
+		b, err := io.ReadAll(c.Request().Body)
+		if err != nil {
+			c.Logger().Errorf("error getting body %v", err)
+		}
+
+		fmt.Println("---->", string(b))
+
+		return c.Render(http.StatusOK, nil)
+	})
 	root.ServeFiles("/", http.FS(public.FS()))
 }
